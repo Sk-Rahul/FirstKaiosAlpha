1d87456983113cfcda3602e80a2ee10387a433a4
Sk-Rahul <55654071+Sk-Rahul@users.noreply.github.com>
2019-10-05 09:55:22 +0530

    Update code0.js

--- code0.js
+++ code0.js
@@ -39,32 +39,12 @@ gdjs.NewSceneCode.GDNewObjectObjects1.createFrom(runtimeScene.getObjects("NewObj
 
 
 }; //End of gdjs.NewSceneCode.eventsList0xb43b0
-function handleKeydown(e) {
-    switch(e.key) {
-      case 'ArrowUp':
-        nav(-1);
-        break;
-      case 'ArrowDown':
-            alert("hello");
-        nav(1);
-        break;
-      case 'ArrowRight':
-        nav(1);
-        break;
-      case 'ArrowLeft':
-        nav(-1);
-        break;
-    }
-  }
-  document.activeElement.addEventListener('keydown', handleKeydown);
-  function nav (move) {
-    const currentIndex = document.activeElement.tabIndex;
-    const next = currentIndex + move;
-    const items = document.querySelectorAll('.items');
-    const targetElement = items[next];
-    targetElement.focus();
-  }
-
+function Start(event)
+{
+    x = event.which;
+    alert(x);
+}
+document.addEventListener("keydown",Start);
 
 
 
